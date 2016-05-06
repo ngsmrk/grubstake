a8e1799c14a685cfab26121170422b9bc4503251
Angus Mark <ngsmrk@gmail.com>
2016-05-06 15:44:28 +0100

    Now does list search

--- app/controllers/trello_controller.rb
+++ app/controllers/trello_controller.rb
@@ -1,33 +1,63 @@
 class TrelloController < ApplicationController
+
+  before_filter :init
+
   def form
-  	# @boards = boards
   end
 
   def search
   	@board_name   = params['board_name']
-  	@card_details = card_details(cards(@board_name))
-  	puts @card_details
-  	# @boards = boards  	
+    @list_names   = params['list_names']
+    @time_period  = params['time_period']    
+
+    puts 'Starting to fetch card details'
+
+  	@card_details = []
+    @list_names.split(',').each do | list_name |
+      puts "Fetching card details for board: #{@board_name}, list: #{list_name} and time: #{@time_period}"      
+      @card_details.concat card_details(cards(@board_name, list_name, @time_period))
+    end
+    
+    puts 'Card details fetched'
   end
 
   private
+
+  def init
+    default_list_names
+    time_periods
+    default_board_name
+  end
+
+  def default_list_names
+    @default_list_names = ['Done (Done)', 'Validating Learning', 'Waiting to Deploy [2]', 'In Review[4]', 'In Progress[7]']    
+  end 
+
+  def time_periods
+    @default_time_periods = ['day', 'week', 'month']    
+  end 
+
+  def default_board_name
+    @default_board_name = 'Cornetto'
+  end
+
   def boards
   	boards = Trello::Board.all
   	boards.map { | board | board.name }
   end
 
-  def cards(board_name)
-    opts =  {cards_limit:4}
-    query = "board:\"#{board_name}\" edited:week"
+  def cards(board_name, list_name, time_period)
+    opts =  {cards_limit:20}
+    query = "board:\"#{board_name}\" list:\"#{list_name}\" edited:#{time_period}"
     Trello::Action.search(query,opts)['cards']
   end
 
   def card_details(cards)
     card_details = []
-	cards.each do | card |
-		output = {list: card.list.name, name: card.name, members: card.members.map(&:full_name), url: card.url, text: card.desc, archived: card.closed?, last_modified: card.last_activity_date}
-		card_details << output
-	end
+	  cards.each do | card |
+		  output = {list: card.list.name, name: card.name, members: card.members.map(&:full_name), url: card.url, text: card.desc, archived: card.closed?, last_modified: card.last_activity_date}
+		  card_details << output
+	  end
 
 	card_details
   end
