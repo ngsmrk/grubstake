f734d7aa9e97b3211894acecc6ab52497150ff8a
Angus Mark <ngsmrk@gmail.com>
2016-05-06 14:44:46 +0100

    v1

--- app/controllers/trello_controller.rb
+++ app/controllers/trello_controller.rb
@@ -4,7 +4,9 @@ class TrelloController < ApplicationController
   end
 
   def search
-  	@board_name = params['board_name']
+  	@board_name   = params['board_name']
+  	@card_details = card_details(cards(@board_name))
+  	puts @card_details
   	@boards = boards  	
   end
 
@@ -12,5 +14,21 @@ class TrelloController < ApplicationController
   def boards
   	boards = Trello::Board.all
   	boards.map { | board | board.name }
+  end
+
+  def cards(board_name)
+    opts =  {cards_limit:4}
+    query = "board:\"#{board_name}\" edited:week"
+    Trello::Action.search(query,opts)['cards']
+  end
+
+  def card_details(cards)
+    card_details = []
+	cards.each do | card |
+		output = {list: card.list.name, name: card.name, members: card.members.map(&:full_name), url: card.url, text: card.desc, archived: card.closed?, last_modified: card.last_activity_date}
+		card_details << output
+	end
+
+	card_details
   end
 end
